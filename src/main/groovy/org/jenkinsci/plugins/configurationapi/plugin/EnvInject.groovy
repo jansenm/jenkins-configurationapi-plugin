007b3d1f833385b61ee9383e31cd527049211166
Michael Jansen <info@michael-jansen.biz>
2016-02-24 01:00:58 +0100

    Finish envinject plugin support.

---
+++ src/main/groovy/org/jenkinsci/plugins/configurationapi/plugin/EnvInject.groovy
@@ -0,0 +1,78 @@
+package org.jenkinsci.plugins.configurationapi.plugin
+
+import hudson.Extension
+import hudson.PluginWrapper
+import hudson.slaves.NodeProperty
+import hudson.slaves.NodePropertyDescriptor
+import hudson.util.DescribableList
+import jenkins.model.GlobalConfiguration
+import jenkins.model.Jenkins
+import org.jenkinsci.plugins.configurationapi.NodeConfigurationStream
+import org.jenkinsci.plugins.configurationapi.PluginConfigurationStream
+import org.jenkinsci.plugins.envinject.EnvInjectNodeProperty
+import org.jenkinsci.plugins.envinject.EnvInjectPluginConfiguration
+
+@Extension
+class EnvInject implements NodeConfigurationStream, PluginConfigurationStream
+{
+    /*
+     * NODE
+     */
+
+    @Override
+    void doImport(Jenkins jenkins, DescribableList<NodeProperty<?>, NodePropertyDescriptor> nodeProperties, Map
+            configuration)
+    {
+        EnvInjectNodeProperty envInject = new EnvInjectNodeProperty(
+                (boolean) configuration['unsetSystemVariables'],
+                (String) configuration['propertiesFilePath'])
+        nodeProperties.replace(envInject)
+    }
+
+    @Override
+    Map doExport(Jenkins instance, NodeProperty nodeProperty)
+    {
+        EnvInjectNodeProperty property = (EnvInjectNodeProperty) nodeProperty
+        return [
+                'unsetSystemVariables': property.isUnsetSystemVariables(),
+                'propertiesFilePath'  : property.getPropertiesFilePath()
+        ]
+    }
+
+    @Override
+    String getNodePropertyClass()
+    {
+        return EnvInjectNodeProperty.getName()
+    }
+
+    /*
+     * PLUGIN
+     */
+
+    @Override
+    String getPluginId()
+    {
+        return 'envinject'
+    }
+
+    @Override
+    Map doExport(Jenkins jenkins, PluginWrapper plugin)
+    {
+        EnvInjectPluginConfiguration config = EnvInjectPluginConfiguration.getInstance()
+        return [
+                hideInjectedVars : config.isHideInjectedVars(),
+                enablePermissions: config.isEnablePermissions()
+        ]
+    }
+
+    @Override
+    void doImport(Jenkins jenkins, Map configuration)
+    {
+        GlobalConfiguration.all().remove(EnvInjectPluginConfiguration.getInstance())
+        // But how to activate it?
+        GlobalConfiguration.all().add(new EnvInjectPluginConfiguration(
+                (boolean) configuration.hideInjectedVars,
+                (boolean) configuration.enablePermissions
+        ))
+    }
+}
